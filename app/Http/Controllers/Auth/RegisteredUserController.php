f8430305d0d122817af7a0b43e85c4fee1fe1389
Soburr <adebesinbenzenejr99@gmail.com>
2023-04-08 23:30:46 +0100

    feature/authentication

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -33,7 +33,7 @@ class RegisteredUserController extends Controller
         $request->validate([
             'name' => ['required', 'string', 'max:255'],
             'number' => 'required|digits:11',
-            'hallOfResidence ' => ['required', 'string', 'max:255'],
+            'hallOfResidence' => ['required', 'string', 'max:255'],
             'roomNumber' => ['required', 'string', 'max:255'],
             'email' => ['required', 'string', 'email', 'max:255', 'unique:'.User::class],
             'password' => ['required', 'confirmed', Rules\Password::min(8)->mixedCase()],
